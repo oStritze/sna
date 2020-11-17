02af512a65e16bab18fc4b130dc670b656f059e6
RaphaelScho <noemail@email.no>
2020-11-17 16:55:53 +0100

    Implemented first look at Inland politics interactions

--- src/read_graph.py
+++ src/read_graph.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import networkx as nx
+from collections import Counter
 
 
 def get_articles_shared_users(postings=None):
@@ -14,7 +15,7 @@ def get_articles_shared_users(postings=None):
 	return nx.from_pandas_edgelist(postings_merged, source="ID_Article_x", target="ID_Article_y", edge_attr=True)
 
 
-def get_users_voted_other_users(joined=None, positive_vote=True):
+def get_users_voted_other_users(joined=None, positive_vote=True, multi_di_graph=False):
 	if joined is None:
 		postings = read_all_postings()
 		votes = read_all_votes()
@@ -22,28 +23,34 @@ def get_users_voted_other_users(joined=None, positive_vote=True):
 
 	joined = joined[["ID_CommunityIdentity_p", "ID_Posting", "ID_CommunityIdentity_v", "VoteNegative", "VotePositive"]] \
 		.query("{} == 1".format("VotePositive" if positive_vote else "VoteNegative"))
-	return nx.from_pandas_edgelist(joined, source="ID_CommunityIdentity_v", target="ID_CommunityIdentity_p", create_using=nx.DiGraph)
+	if multi_di_graph:
+		return nx.from_pandas_edgelist(joined, source="ID_CommunityIdentity_v", target="ID_CommunityIdentity_p", create_using=nx.MultiDiGraph)
+	else:
+		return nx.from_pandas_edgelist(joined, source="ID_CommunityIdentity_v", target="ID_CommunityIdentity_p", create_using=nx.DiGraph)
 
 
-def get_users_commented_other_users(postings=None):
+def get_users_commented_other_users(postings=None, multi_di_graph=False):
 	if postings is None:
 		postings = read_all_postings()
 
 	parent_postings = postings.query("ID_Posting_Parent == 'NaN'")
 	child_postings = postings.query("ID_Posting_Parent != 'NaN'")
 	postings_joined = parent_postings.merge(child_postings, left_on="ID_Posting", right_on="ID_Posting_Parent", suffixes=("_parent", "_child"))
-	return nx.from_pandas_edgelist(postings_joined, source="ID_CommunityIdentity_child", target="ID_CommunityIdentity_parent", create_using=nx.DiGraph)
+	if multi_di_graph:
+		return nx.from_pandas_edgelist(postings_joined, source="ID_CommunityIdentity_child", target="ID_CommunityIdentity_parent", create_using=nx.MultiDiGraph)
+	else:
+		return nx.from_pandas_edgelist(postings_joined, source="ID_CommunityIdentity_child", target="ID_CommunityIdentity_parent", create_using=nx.DiGraph)
 
 
-def get_all_users_interactions(postings=None, votes=None):
+def get_all_users_interactions(postings=None, votes=None, multi_di_graph=False):
 	if postings is None or votes is None:
 		postings = read_all_postings()
 		votes = read_all_votes()
 
 	joined = postings.merge(votes, on="ID_Posting", suffixes=("_p", "_v"))
-	positives = get_users_voted_other_users(joined, positive_vote=True)
-	negatives = get_users_voted_other_users(joined, positive_vote=False)
-	comments = get_users_commented_other_users(postings)
+	positives = get_users_voted_other_users(joined, positive_vote=True, multi_di_graph=multi_di_graph)
+	negatives = get_users_voted_other_users(joined, positive_vote=False, multi_di_graph=multi_di_graph)
+	comments = get_users_commented_other_users(postings, multi_di_graph=multi_di_graph)
 	return nx.disjoint_union_all([positives, negatives, comments])
 
 
@@ -56,3 +63,11 @@ def read_all_votes():
 	votes = pd.read_csv("../data/raw/Votes_01052019_15052019.csv", delimiter=";")
 	return pd.concat([votes, pd.read_csv("../data/raw/Votes_16052019_31052019.csv", delimiter=";")])
 
+def get_weighted_interaction_graph(postings, votes, thresh=0):
+	G = get_all_users_interactions(postings, votes, multi_di_graph=True)
+	width_dict = Counter(G.edges())
+	edge_width = [(u, v, value) for ((u, v), value) in width_dict.items()]
+	all_int_weighted = pd.DataFrame(edge_width)
+	all_int_weighted.columns = ["user1", "user2", "weight"]
+	all_int_weighted_high = all_int_weighted[all_int_weighted.weight >= thresh]
+	return nx.from_pandas_edgelist(all_int_weighted_high, source="user1", target="user2", edge_attr=True)
