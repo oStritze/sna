9be6eccbfab79041a23ad3bc16d2cb325f4b330e
RaphaelScho <noemail@email.no>
2020-11-17 16:55:22 +0100

    Allowed for setting node_size in plot_colored_graph()

--- src/viz.py
+++ src/viz.py
@@ -9,7 +9,7 @@ def plot_graph(G: nx.Graph, figsize=(20, 20), with_labels=True, arrows=True):
 
 
 def plot_colored_graph(G: nx.Graph, node_color_attr=None, edge_color_attr=None, edge_width_factor=None, pos=None, figsize=(20, 20),
-					   node_cmap=plt.cm.plasma, edge_cmap=plt.cm.viridis, node_ticks=None, edge_ticks=None):
+					   node_cmap=plt.cm.plasma, edge_cmap=plt.cm.viridis, node_ticks=None, edge_ticks=None, node_size=300):
 	node_colors = None if node_color_attr is None else [G.nodes()[n][node_color_attr] for n in G.nodes()]
 	edge_colors = None if edge_color_attr is None else [G.edges()[e][edge_color_attr] for e in G.edges()]
 	edge_widths = 1 if edge_width_factor is None else [c / max(edge_colors) * edge_width_factor for c in edge_colors]
@@ -18,7 +18,7 @@ def plot_colored_graph(G: nx.Graph, node_color_attr=None, edge_color_attr=None, 
 	if pos is None:
 		pos = nx.spring_layout(G, seed=7)
 	ec = nx.draw_networkx_edges(G, pos, edge_color=edge_colors, edge_cmap=edge_cmap, width=edge_widths)
-	nc = nx.draw_networkx_nodes(G, pos, node_color=node_colors, cmap=node_cmap)
+	nc = nx.draw_networkx_nodes(G, pos, node_color=node_colors, cmap=node_cmap, node_size=node_size)
 
 	if node_color_attr is not None:
 		if node_ticks is None:
